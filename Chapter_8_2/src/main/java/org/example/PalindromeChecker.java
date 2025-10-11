144600a0cccdaec7aad90bc1419ebee7986c92e9
Taysa Abinader <ferreira.taysa@gmail.com>
2025-10-11 15:04:11 +0300

    Chapter 8.2 Task 1: PalindromeChecker implementation

--- Chapter_8_2/src/main/java/org/example/PalindromeChecker.java
+++ Chapter_8_2/src/main/java/org/example/PalindromeChecker.java
@@ -1,8 +1,46 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class PalindromeChecker {
     public boolean isPalindrome(String str) {
-        // TODO
-        return false;
+        // Error checks: string is null or empty.
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+
+        int length = str.length();
+
+        // A single character is always a palindrome.
+        if (length == 1) {
+            return true;
+        }
+
+        ArrayList<Character> ignoredChars = new ArrayList<>();
+        ignoredChars.add(' '); // whitespace
+        ignoredChars.add(','); // comma
+
+        int left = 0;
+        int right = length - 1;
+        while (left < right) {
+            // Skip ignored characters to the left.
+            if (ignoredChars.contains(str.charAt(left))) {
+                left++;
+                continue;
+            }
+            // Skip ignored characters to the right.
+            if (ignoredChars.contains(str.charAt(right))) {
+                right--;
+                continue;
+            }
+            // Convert all characters to lowercase when comparing.
+            if (Character.toLowerCase(str.charAt(left)) != Character.toLowerCase((str.charAt(right)))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
--- Chapter_8_2/src/test/java/PalindromeCheckerTest.java
+++ Chapter_8_2/src/test/java/PalindromeCheckerTest.java
@@ -10,7 +10,10 @@ public class PalindromeCheckerTest {
         PalindromeChecker checker = new PalindromeChecker();
 
         assertTrue(checker.isPalindrome("radar"));
+
+        // Special case: skip whitespace.
         assertTrue(checker.isPalindrome("A man, a plan, a canal, Panama"));
+
         assertFalse(checker.isPalindrome("hello"));
         assertFalse(checker.isPalindrome("openai"));
     }
