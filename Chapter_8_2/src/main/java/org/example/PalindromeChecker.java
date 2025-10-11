35eee4d6c6eed3d62b3196e05b613aa9ba76ebd7
Taysa Abinader <ferreira.taysa@gmail.com>
2025-10-11 14:38:38 +0300

    Chapter 8.2 Task 1: PalindromeChecker skeleton

---
+++ Chapter_8_2/src/main/java/org/example/PalindromeChecker.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class PalindromeChecker {
+    public boolean isPalindrome(String str) {
+        // TODO
+        return false;
+    }
+}
---
+++ Chapter_8_2/src/test/java/PalindromeCheckerTest.java
@@ -0,0 +1,17 @@
+import org.example.PalindromeChecker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PalindromeCheckerTest {
+    @Test
+    public void testIsPalindrome() {
+        PalindromeChecker checker = new PalindromeChecker();
+
+        assertTrue(checker.isPalindrome("radar"));
+        assertTrue(checker.isPalindrome("A man, a plan, a canal, Panama"));
+        assertFalse(checker.isPalindrome("hello"));
+        assertFalse(checker.isPalindrome("openai"));
+    }
+}
