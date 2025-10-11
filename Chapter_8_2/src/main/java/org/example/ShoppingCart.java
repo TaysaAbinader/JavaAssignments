580243c2e1809d2f7009f8e06fb05bf384147942
Taysa Abinader <ferreira.taysa@gmail.com>
2025-10-11 15:07:42 +0300

    Chapter 8.2 Task 2: ShoppingCart skeleton

---
+++ Chapter_8_2/src/main/java/org/example/ShoppingCart.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class ShoppingCart {
+
+    public void addItem(String apple, double v) {
+        // TODO
+    }
+
+    public void removeItem(String apple) {
+        // TODO
+    }
+
+    public int getItemCount() {
+        // TODO
+        return 0;
+    }
+
+    public double calculateTotal() {
+        // TODO
+        return 0.0;
+    }
+}
